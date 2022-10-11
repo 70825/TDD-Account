36a39a67a196118cd59eaa7b48532fcb72b36373
Dabeen Jeong <hello70825@gmail.com>
2022-10-11 13:28:30 +0900

    add getBalance test

--- src/test/java/practice/TDDAccount/AccountTest.java
+++ src/test/java/practice/TDDAccount/AccountTest.java
@@ -10,4 +10,16 @@ class AccountTest {
     public void testAccount() throws Exception{
         Account account = new Account(10000);
     }
+
+    @Test
+    public void testGetBalance() throws Exception {
+        Account account = new Account(10000);
+        assertEquals(10000, account.getBalance());
+
+        account = new Account(1000);
+        assertEquals(1000, account.getBalance());
+
+        account = new Account(0);
+        assertEquals(0, account.getBalance());
+    }
 }
