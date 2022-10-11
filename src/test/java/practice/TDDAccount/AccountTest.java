500105f7b7bd4abf6d937219cf3fedb0599d2d9b
Dabeen Jeong <hello70825@gmail.com>
2022-10-11 13:40:24 +0900

    refactor unit test code

--- src/test/java/practice/TDDAccount/AccountTest.java
+++ src/test/java/practice/TDDAccount/AccountTest.java
@@ -1,19 +1,24 @@
 package practice.TDDAccount;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountTest {
+    private Account account;
+
+    @BeforeEach
+    private void setup() {
+        account = new Account(10000);
+    }
 
     @Test
     public void testAccount() throws Exception{
-        Account account = new Account(10000);
     }
 
     @Test
     public void testGetBalance() throws Exception {
-        Account account = new Account(10000);
         assertEquals(10000, account.getBalance());
 
         account = new Account(1000);
@@ -25,14 +30,12 @@ class AccountTest {
 
     @Test
     public void testDeposit() throws Exception {
-        Account account = new Account(10000);
         account.deposit(1000);
         assertEquals(11000, account.getBalance());
     }
 
     @Test
     public void testWithdraw() throws Exception {
-        Account account = new Account(10000);
         account.withdraw(1000);
         assertEquals(9000, account.getBalance());
     }
